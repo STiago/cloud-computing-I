e99d6cb6218e4bddbb9e35d9446fa679967030f4
STiago <victoriasantiago@correo.ugr.es>
2018-01-18 21:10:57 +0000

    Deleted old files

--- orquestacion/aws-credentials.sh
+++
@@ -1,5 +0,0 @@
-export AWS_KEY=clave
-export AWS_SECRET=clave
-export AWS_SECURITY_GROUP_NAME=grupo
-export AWS_KEYNAME=fichero
-export AWS_KEYPATH=ruta
--- provision/chef/cookbooks/nano/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-apt_update 'all platforms'
-
-package 'mongodb'
-package 'python-setuptools'
-package 'python-dev'
-package 'python-pip'
-
-easy_install_package 'python-telegram-bot'
